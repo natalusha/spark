2a2f234810e385898409312fad47ccab7906b097
nataliia holubtsova <natalyholubtsova@gmail.com>
2022-04-03 20:32:41 +0300

    2nd_task.py added

---
+++ 2nd_task.py
@@ -0,0 +1,46 @@
+"""Лучшие фильмы нам известны, а что если ты захочешь посмотреть, ну, скажем лучший триллер всех времен и народов.Т
+ак вот нужно найти по топ-10 фильмов каждого жанра (результат должен быть в одном файлике =))"""
+from pyspark.sql import SparkSession
+from pyspark.sql.window import Window
+from pyspark.sql.functions import dense_rank, col
+
+spark = SparkSession.builder.appName("TotalOrdersPErRegionCountry").getOrCreate()
+rating_file = "imdbdata/title.ratings.tsv/data.tsv"
+rating_df = (
+    spark.read.format("csv")
+    .option("header", "true")
+    .option("sep", "\t")
+    .option("inferSchema", "true")
+    .load(rating_file)
+)
+title_file = "imdbdata/title.basics.tsv/data.tsv"
+title_df = (
+    spark.read.format("csv")
+    .option("header", "true")
+    .option("sep", "\t")
+    .option("inferSchema", "true")
+    .load(title_file)
+)
+
+windowSpec = Window.partitionBy(title_df["genres"]).orderBy(
+    rating_df["averageRating"].desc()
+)
+
+top10genres = (
+    rating_df.join(title_df, rating_df.tconst == title_df.tconst, "inner")
+    .select(
+        rating_df["tconst"],
+        title_df["primaryTitle"],
+        title_df["startYear"],
+        title_df["genres"],
+        rating_df["averageRating"],
+        rating_df["numVotes"],
+    )
+    .filter((title_df.titleType == "movie") & (rating_df.numVotes > 100000))
+    .withColumn("dense_rank", dense_rank().over(windowSpec))
+    .sort("genres", "dense_rank")
+)
+top10genres.filter(col("dense_rank") <= 10).drop("dense_rank")
+top10genres.coalesce(1).write.option("header", "true").option(
+    "inferSchema", "true"
+).csv("output/top10genres")
