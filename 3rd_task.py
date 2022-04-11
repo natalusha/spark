7bca6e2ca3a4028ec343e353d16bbf7638d17b0a
nataliia holubtsova <natalyholubtsova@gmail.com>
2022-04-11 20:08:19 +0300

    explode added, sort changed

--- 3rd_task.py
+++ 3rd_task.py
@@ -2,7 +2,7 @@
 *нужно найти по топ-10 фильмов каждого жанра для каждого десятилетия c сейчас до 1950х"""
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import substring, expr
-from pyspark.sql.functions import dense_rank
+from pyspark.sql.functions import dense_rank, split, explode
 from pyspark.sql.functions import concat_ws, col, year, current_date
 from pyspark.sql.types import IntegerType
 from pyspark.sql.window import Window
@@ -24,8 +24,8 @@ title_df = (
     .option("inferSchema", "true")
     .load(title_file)
 )
-windowSpec = Window.partitionBy("genres", "yearRange").orderBy(
-    rating_df["averageRating"].desc()
+windowSpec = Window.partitionBy("genre", "yearRange").orderBy(
+    col("yearRange").desc(), "genre", rating_df["averageRating"].desc()
 )
 df = (
     rating_df.join(title_df, rating_df.tconst == title_df.tconst, "inner")
@@ -33,7 +33,7 @@ df = (
         rating_df["tconst"],
         title_df["primaryTitle"],
         title_df["startYear"],
-        title_df["genres"],
+        (explode(split(title_df.genres, ",")).alias("genre")),
         rating_df["averageRating"],
         rating_df["numVotes"],
     )
@@ -42,14 +42,15 @@ df = (
         & (rating_df.numVotes > 100000)
         & (title_df.startYear.between(1950, year(current_date())))
     )
-    .sort(col("genres"), col("averageRating").desc())
+    .sort(col("averageRating").desc())
 )
 
 nullyear_df = (
-    df.withColumn("nullYear", expr("substring(startYear,1,3)||'0'").cast(IntegerType()))
-    .withColumn("yearRange", concat_ws(" - ", col("nullYear"), col("nullYear") + 10))
+    df.withColumn("nullYear", expr("substring(startYear,1,3)||'1'").cast(IntegerType()))
+    .withColumn("yearRange", concat_ws(" - ", col("nullYear"), col("nullYear") + 9))
     .withColumn("dense_rank", dense_rank().over(windowSpec))
     .filter(col("dense_rank") <= 10)
+    .sort(col("yearRange").desc(), col("genre"), col("averageRating").desc())
 )
 res = nullyear_df.drop("nullYear", "dense_rank")
 res.coalesce(1).write.option("header", "true").option("inferSchema", "true").csv(
